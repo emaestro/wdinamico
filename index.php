be16db700dcea7d29e8ee44da3bcebb8a2e31532
Fredy <fredy.c.martinez@unap.pe>
2014-05-13 15:44:15 -0500

    Ordenamos index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-
 	require("cabecera.php");
 ?>
-			<div class="starter-template">
-				<h1>Bootstrap starter template</h1>
-				<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-				<a href="http://google.com" class="btn">Ir a Google</a>
-			</div>
+	<div class="starter-template">
+		<h1>Bootstrap starter template</h1>
+		<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+		<a href="http://google.com" class="btn">Ir a Google</a>
+	</div>
 
