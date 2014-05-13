3d77340325ceb94a3b64726e223e82f4b079d198
Fredy <fredy.c.martinez@unap.pe>
2014-05-13 15:42:38 -0500

    Agregamos los archivos Base

---
+++ pie.php
@@ -0,0 +1,8 @@
+		</div><!-- /.container -->
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/sesion7.js"></script>
+	</body>
+</html>
