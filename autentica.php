aa97609c98a1b8b1b8ef7f41769834845803a198
Fredy <fredy.c.martinez@unap.pe>
2014-05-21 16:49:56 -0500

    Agregamos funciones.php y mysql.php para trabajar con base de datos

--- autentica.php
+++ autentica.php
@@ -1,4 +1,7 @@
 <?php
+
+	require_once("inc/mysql.php");
+
 	$usuario = $_POST['txt-usuario'];
 	$clave = $_POST['txt-clave'];
 	$boton = $_POST['btn-aceptar'];
@@ -6,5 +9,11 @@
 	echo $usuario;
 	echo $clave;
 	echo $boton;
+
+	$sConsulta	=	"SELECT NOW()";
+	$oFecha	=	mysqli($sConsulta);
+
+	$aFecha = $oFecha->fetch_array();
+	print_r($aFecha);
 
 ?>
