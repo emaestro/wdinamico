99475811b57166e647ba1c7f310742b80e120f8c
Fredy <fredy.c.martinez@unap.pe>
2014-05-21 18:03:42 -0500

    Agregamos autenticacion basica

--- autentica.php
+++ autentica.php
@@ -6,14 +6,60 @@
 	$clave = $_POST['txt-clave'];
 	$boton = $_POST['btn-aceptar'];
 
+	/*
 	echo $usuario;
 	echo $clave;
 	echo $boton;
+	*/
+	
+	//$sConsulta	=	"SELECT NOW() as fechita";
+	//$oFecha	=	mysqli($sConsulta);
 
-	$sConsulta	=	"SELECT NOW() as fechita";
-	$oFecha	=	mysqli($sConsulta);
+	//$aFecha = $oFecha->fetch_assoc();
+	//print_r($aFecha);
 
-	$aFecha = $oFecha->fetch_assoc();
-	print_r($aFecha);
+	$sConsulta	=	"SELECT u.* "
+				.	"FROM usuarios u "
+				.	"WHERE u.usuario = '{$usuario}'";
+	if ($oUsuario	=	mysqli($sConsulta)) {
+
+		//Almacenamos la cantidad de resultados (rows)
+		$nUsuario	=	$oUsuario->num_rows;
+		
+		//Cuando solo existe un usuario
+		if ($nUsuario == 1) {
+			$aUsuario = $oUsuario->fetch_assoc();
+			if ($aUsuario['clave']==$clave) {
+				header("Location:bienvenido.php");
+			}
+			else
+			{
+?>
+				<div class="alert alert-error">
+					<p><strong>Error!!!</strong>Contraseña incorrecta?></p>
+				</div>
+<?php
+
+			}
+		}
+		else
+		{
+?>
+			<div class="alert alert-error">
+				<p><strong>Error!!!</strong>Existe mas de un usuario para <?=$usuario?></p>
+			</div>
+<?php
+		}
+	}else{
+?>
+		<div class="alert alert-error">
+			<p><strong>Error!!!</strong> No fue posible realizar la autenticación</p>
+		</div>
+<?php
+	}
+
+
+	
+
 
 ?>
