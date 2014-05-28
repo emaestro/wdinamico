462153bad172cb4d1f950dedc8543130aeb0af92
Fredy <fredy.c.martinez@unap.pe>
2014-05-28 16:50:26 -0500

    Agregamos listado de usuarios

--- listado.php
+++ listado.php
@@ -1,25 +1,71 @@
 <?php
 	require("cabecera.php");
+	require_once("inc/mysql.php");
 
 	if (isset($_SESSION['usuario']['autenticado']) && $_SESSION['usuario']['autenticado']) {
+
+		$sSQL	= 	"SELECT u.* "
+				.	"FROM usuarios u "
+				.	"ORDER BY u.paterno, u.materno, u.nombres ";
+
+		if ($oUsuario	=	mysqli($sSQL)) {
+			$ok = TRUE;
+		}else{
+			$ok = FALSE;
+			$_SESSION['mensaje'] = "Error al consultar usuarios";
+			header("Location:nuevo.php");
+		}
 	}else{
 		header("Location:acceso.php");
 	}
 
 	require("menu.php");
+
 ?>
 
 	<div class="container">
 
 		<div class="row">
-			<div class="col-md-offset-4 col-md-4">
+			<div class="col-md-12">
 
-				<div class="alert alert-info">
-					<p>Bienvenido al sitio seguro</p>
-					<?php
-						echo $_SESSION['usuario']['usuario'];
-					?>
-				</div>
+<?php
+				if ($ok) {
+?>
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Apellidos y Nombres</th>
+								<th>Usuario</th>
+								<th>Operaciones</th>
+							</tr>				
+						</thead>
+						<tbody>
+<?php
+							while ($aUsuario = $oUsuario->fetch_assoc()) {
+?>
+								<tr>
+									<td><?php echo $aUsuario['paterno']," ",$aUsuario['materno']," ",$aUsuario['nombres'];?></td>
+									<td><?php echo $aUsuario['usuario'];?></td>
+									<td></td>
+								</tr>
+<?php
+							}
+?>				
+						</tbody>
+						<tfoot>
+					
+						</tfoot>
+					</table>
+<?php
+				}else{
+?>
+					<div class="alert alert-error">
+						<?php echo $_SESSION['mensaje']; unset($_SESSION['mensaje']);?>
+						
+					</div>
+<?php
+				}
+?>
 				
 			</div>
 		</div>
