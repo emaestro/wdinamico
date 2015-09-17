7f268f5f48e701cb2afb7b95308fd92eabafab26
emaestro <f@emaestro.net>
2015-09-17 18:31:21 -0500

    Agregamos los archivos de fpdf como herramientas para generar reportes en archivos PDF

--- usuario.sql
+++ usuario.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2015 a las 01:26:07
+-- Tiempo de generación: 18-09-2015 a las 01:27:38
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -17,19 +17,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `test`
---
---
 -- Base de datos: `wdinamico`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuario` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario_id` smallint(5) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `paterno` varchar(30) NOT NULL,
   `materno` varchar(30) NOT NULL,
@@ -38,7 +35,14 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `clave` varchar(32) NOT NULL,
   `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`usuario_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `paterno`, `materno`, `nombres`, `usuario`, `clave`, `fecha_registro`) VALUES
+(00001, 'GARCIA', 'PEREZ', 'ALAN', 'alan', 'garcia', '2015-09-17 23:22:48');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
